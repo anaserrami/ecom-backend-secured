a2cf6174cd8e0dd88de4658f1dca58e306621c8c
anaserrami <ramianas314@gmail.com>
2024-12-25 22:15:37 +0100

    add ProductItem entity

---
+++ orders-service/src/main/java/com/errami/ordersservice/entities/ProductItem.java
@@ -0,0 +1,22 @@
+package com.errami.ordersservice.entities;
+
+import com.errami.ordersservice.models.Product;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor @Getter @Setter @Builder @ToString
+public class ProductItem {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productId;
+    private double price;
+    private int quantity;
+    @ManyToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Order order;
+
+    @Transient
+    private Product product;
+}
