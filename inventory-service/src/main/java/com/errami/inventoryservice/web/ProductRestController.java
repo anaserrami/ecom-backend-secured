b1fbeb825467aefcdf09ba6b91903498c1fa054f
anaserrami <ramianas314@gmail.com>
2024-12-25 21:55:09 +0100

    add web layer

---
+++ inventory-service/src/main/java/com/errami/inventoryservice/web/ProductRestController.java
@@ -0,0 +1,32 @@
+package com.errami.inventoryservice.web;
+
+import com.errami.inventoryservice.entities.Product;
+import com.errami.inventoryservice.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class ProductRestController {
+    private final ProductRepository productRepository;
+
+    @GetMapping("/products")
+    //@PreAuthorize("hasAuthority('ADMIN')")
+    public List<Product> productList(){
+        return productRepository.findAll();
+    }
+    @GetMapping("/products/{id}")
+    //@PreAuthorize("hasAuthority('USER')")
+    public Product productById(@PathVariable String id){
+        return productRepository.findById(id).get();
+    }
+    @GetMapping("/auth")
+    public Authentication authentication(Authentication authentication){
+        return authentication;
+    }
+}
